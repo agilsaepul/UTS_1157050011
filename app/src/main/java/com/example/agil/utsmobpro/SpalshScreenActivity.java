a40757a0c05e42191c3a07690068c3834fb5e40b
agilsaepul <agil.gilbro69@gmail.com>
2018-11-05 15:41:26 +0700

    versi alpha

---
+++ app/src/main/java/com/example/agil/utsmobpro/SpalshScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.agil.utsmobpro;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SpalshScreenActivity extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh_screen);
+
+        setContentView(R.layout.activity_spalsh_screen);
+
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
